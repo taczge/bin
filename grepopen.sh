1eda9ce60d4a9f9545233db539d8cbad79c9758c
tn <tn00gm@gmail.com>
2015-05-12 13:38:16 +0900

    remove -euxv option.

--- grepopen.sh
+++ grepopen.sh
@@ -1,13 +1,11 @@
-#!/bin/bash -euxv
+#!/bin/bash
 
 if [ $# -ne 1 ]; then
     echo "usage: $0 [REGEX]"
     exit 0
 fi
 
-regex=$1
 ag_line=$(ag $1 | peco)
-
 file=$(echo $ag_line | cut -d: -f1)
 line=$(echo $ag_line | cut -d: -f2)
 
